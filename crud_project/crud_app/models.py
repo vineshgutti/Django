165e47259d3016fdbaebabad0828f9a94af7acc8
vinesh kumar <vineshkumargutti309@gmail.com>
2023-03-03 16:42:54 +0530

    first commit

---
+++ crud_project/crud_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'product'
+        
+    def __str__(self):
+        return self.name
---
+++ crud_project/crud_app/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render,redirect
+from .models import Product
+from .forms import ProductForm
+
+# Create your views here.
+def crate(request):
+    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('details')
+        else:
+            form = ProductForm()
+    else:
+        return render(request, 'crud_app/form.html', {'form':form})
+    
+def details(request):
+    data = Product.objects.all()
+    return render(request, 'crud_app/details.html', {'data':data})
+
+def update(request,id):
+    object=Product.objects.get(id=id)
+    form = ProductForm(instance=object)
+    if request.method == 'POST':
+        form=ProductForm(request.POST,instance=object)
+        if form.is_valid:
+            form.save()
+            return redirect('details')
+    else:
+        return render(request,'crud_app/update.html',{'form':form})
+    
+def delete(request,id):   
+        Product.objects.get(id=id).delete()
+        return redirect('details')
+    
