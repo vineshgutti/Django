1e7f1af9bb4f83e191019716f449880c49a42387
vinesh kumar <vineshkumargutti309@gmail.com>
2023-03-23 18:03:30 +0530

    validations applied

--- signup_projct/signupapp/forms.py
+++ signup_projct/signupapp/forms.py
@@ -3,28 +3,29 @@ from .models import Registration
 from django.core.exceptions import ValidationError
 
 class RegistrationForm(forms.ModelForm):
-    firstname = forms.CharField(required=True,help_text='Firstname',widget=forms.TextInput(attrs={'class':'form-control','id':'firstname','placeholder':'firstname'}))
-    lastname = forms.CharField(required=True,help_text='Lastname',widget=forms.TextInput(attrs={'class':'form-control','id':'lastname','placeholder':'lastname'}))
-    email = forms.CharField(required=True,help_text='Email',widget=forms.EmailInput(attrs={'class':'form-control','id':'email','placeholder':'email'}))
-    password = forms.CharField(required=True,help_text='Password',widget=forms.PasswordInput(attrs={'class':'form-control','id':'password','placeholder':'password'}))
-    re_password = forms.CharField(required=True,help_text='Re-Password',widget=forms.PasswordInput(attrs={'class':'form-control','id':'re_password','placeholder':'re_password'}))
+    firstname = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-label','id':'firstname','placeholder':'firstname'}))
+    lastname = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-label','id':'lastname','placeholder':'lastname'}))
+    email = forms.CharField(required=True,widget=forms.EmailInput(attrs={'class':'form-label','id':'email','placeholder':'email'}))
+    username = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-label','id':'username','placeholder':'username'}))
+    password = forms.CharField(required=True,widget=forms.PasswordInput(attrs={'class':'form-label','id':'password','placeholder':'password'}))
+    re_password = forms.CharField(required=True,widget=forms.PasswordInput(attrs={'class':'form-label','id':'re_password','placeholder':'re_password'}))
     class Meta:
         model = Registration
         fields = '__all__'
         
-        def clean(self):
-            clean_data = super().clean()
-            if len(clean_data['firstname']) < 4:
-                raise ValidationError("The firstname length should be minimum '4' characters")
-            if len(clean_data['lastname']) < 4:
-                raise ValidationError("The lastname length should be minimum '4' characters")
-            if '@gmail.com' not in clean_data['email']:
-                raise ValidationError("Please provide valid mail with '@gmail.com'")
-            if len(clean_data['username']) < 8:
-                raise ValidationError("The user name length should be minimum '8' characters")
-            if clean_data['password'] and clean_data['re_password'] < 8:
-                raise ValidationError("password length minimum '8' characters")
-            if clean_data['password'] != clean_data['re_password']:
-                raise ValidationError("passwords should be the same")
-                
+    def clean(self):
+        clean_data = super().clean()
+        if len(clean_data['firstname']) < 4:
+            raise ValidationError("The firstname length should be minimum '4' characters")
+        if len(clean_data['lastname']) < 4:
+            raise ValidationError("The lastname length should be minimum '4' characters")
+        if '@gmail.com' not in clean_data['email']:
+            raise ValidationError("Please provide valid mail with '@gmail.com'")
+        if len(clean_data['username']) < 8:
+            raise ValidationError("The user name length should be minimum '8' characters")
+        if len(clean_data['password']) < 8 and len(clean_data['re_password']) < 8:
+            raise ValidationError("password length minimum '8' characters")
+        if clean_data['password'] != clean_data['re_password']:
+            raise ValidationError("passwords should be matched")
+            
             
--- signup_projct/signupapp/views.py
+++ signup_projct/signupapp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Registration
+from .forms import RegistrationForm
+from django.http import HttpResponse
+# from django.contrib.auth.models import AbstractUser
 
 # Create your views here.
+def home(request):
+    return render(request, 'signupapp/base.html')
+def signup(request):
+    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        print(request.POST)
+        if form.is_valid():
+            print(form.is_valid())
+            form.save()
+            return HttpResponse('Registered successfully')
+        else:
+            form.errors
+    else:
+        form=RegistrationForm()
+    return render(request,'signupapp/signup.html',{'form':form})
